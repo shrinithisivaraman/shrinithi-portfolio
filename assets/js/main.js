4f888320480e78134c8c2c0868bd0e0859861ff2
shrinithisivaraman <shrinithi31sivaraman@gmail.com>
2025-08-27 12:58:59 +0100

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -398,4 +398,32 @@
 						$main._show(location.hash.substr(1), true);
 					});
 
+
 })(jQuery);
+
+function openCategory(evt, categoryName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) tabcontent[i].style.display = "none";
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) tablinks[i].className = tablinks[i].className.replace(" active", "");
+    document.getElementById(categoryName).style.display = "block";
+    evt.currentTarget.className += " active";
+
+    var firstSubtab = document.querySelector('#' + categoryName + ' .subtab .subtablinks');
+    if (firstSubtab) firstSubtab.click();
+}
+
+function openProject(evt, projectName) {
+    var i, subtabcontent, subtablinks;
+    subtabcontent = document.getElementsByClassName("subtabcontent");
+    for (i = 0; i < subtabcontent.length; i++) subtabcontent[i].style.display = "none";
+    subtablinks = document.getElementsByClassName("subtablinks");
+    for (i = 0; i < subtablinks.length; i++) subtablinks[i].className = subtablinks[i].className.replace(" active", "");
+    document.getElementById(projectName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementsByClassName("tablinks")[0].click();
+});
